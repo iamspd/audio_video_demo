a96c580c3734fb18d87bf515a7e7fab417b9b2b8
iamspd <iamspd.ai9@gmail.com>
2021-05-25 21:45:06 -0400

    VideoActivity - attached video to the layout

--- app/src/main/java/com/example/audiovideodemo/VideoActivity.java
+++ app/src/main/java/com/example/audiovideodemo/VideoActivity.java
@@ -2,13 +2,24 @@ package com.example.audiovideodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
+
+    // widgets
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName()
+                + "/" + R.raw.demovideo);
+
+        videoView.start();
     }
 }
