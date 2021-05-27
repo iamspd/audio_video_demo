9117b8d0a9410128552cb5f7979ac45c4367b9b9
iamspd <iamspd.ai9@gmail.com>
2021-05-26 21:40:22 -0400

    AudioActivity - Logic implemented

--- app/src/main/java/com/example/audiovideodemo/AudioActivity.java
+++ app/src/main/java/com/example/audiovideodemo/AudioActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class AudioActivity extends AppCompatActivity {
+
+    // variables
+    private MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_audio);
 
-        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.car_starting);
+        mPlayer = MediaPlayer.create(this, R.raw.car_starting);
+    }
+
+    public void onClickPlay(View view){
 
         mPlayer.start();
+    }
+
+    public void onClickPause(View view){
+
+        mPlayer.pause();
     }
 }
